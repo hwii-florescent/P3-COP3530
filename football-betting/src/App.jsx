8925f4e9cc729d191800fa2fdb3804ad714e0dae
hwii <hghptcnn@gmail.com>
2024-04-06 16:50:49 -0400

    Add search bar

--- football-betting/src/App.jsx
+++ football-betting/src/App.jsx
@@ -1,11 +1,19 @@
 import {useState,useEffect} from 'react'
 import './App.css'
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setuserInput] = useState('');
   const [tabactive, settabactive]= useState('home');
   const funcclickedTab = (clickedtab) =>
   {
     settabactive(clickedtab);
+  }
+
+  const handleChange = (e) => {
+    setuserInput(e.target.value);
+  }
+
+  const searchPlayer = () => {
+    //take value from the dataset and display the player
   }
 
   return (
@@ -33,6 +41,11 @@ function App() {
       {tabactive === 'team' && <div>Team</div>}
       {tabactive === 'individ' && <div>Individual</div>}
     </div>
+    <div className='search-container'>
+      <input type='text' placeholder='Search players...' className='searchbar' value={userInput} onChange={handleChange}/>
+      <button className='searchbutton' onClick={searchPlayer}>Search</button>
+    </div>
+
     </div>
   )
 }
