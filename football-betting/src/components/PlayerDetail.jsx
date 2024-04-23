e0bdc6470246ca93782bea997ff3d7f927b47d5a
hwii <116895905+hwii-florescent@users.noreply.github.com>
2024-04-22 22:08:10 -0400

    Update more stats from player displayed

--- football-betting/src/components/PlayerDetail.jsx
+++ football-betting/src/components/PlayerDetail.jsx
@@ -7,7 +7,7 @@ function PlayerDetail({ name, batch, playerVer }) {
     const values = batch[playerVer];
     content = (
       <div className="player-c">
-        <h2>{name}</h2>
+        <h2>{values[6]}</h2>
         <img
           src={values[values.length - 1]}
           alt={`Image of ${name}`}
@@ -16,8 +16,17 @@ function PlayerDetail({ name, batch, playerVer }) {
         <div className="player-in4">
           <p>Position: {values[7]}</p>
           <p>Overall: {values[8]}</p>
+          <p>Potential: {values[9]}</p>
           <p>Club: {values[20]}</p>
+          <p>Club jersey number: {values[22]}</p>
           <p>Nationality: {values[27]}</p>
+          <p>Preferred foot: {values[31]}</p>
+          <p>Weak foot: {values[32]}</p>
+          <p>Pace: {values[41]}</p>
+          <p>Shooting: {values[42]}</p>
+          <p>Passing: {values[43]}</p>
+          <p>Dribbling: {values[44]}</p>
+          <p>Defending: {values[45]}</p>
         </div>
       </div>
     );
